f6a6f566ca6a6c0cd8e75cd1e401b7b0e74dc6ea
Gibran Hemani <explodecomputer@gmail.com>
2018-03-13 23:07:20 +0000

    updated the docker file to modify shiny server

--- shiny-server.sh
+++ shiny-server.sh
@@ -2,7 +2,7 @@
 
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
-mv shiny-server.conf /etc/shiny-server/
+mv /srv/shiny-server/shiny-server.conf /etc/shiny-server/
 chown shiny.shiny /var/log/shiny-server
 
 exec shiny-server 2>&1
